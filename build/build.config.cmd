79044a465fefbaaeaf8ab2ea37eccdf4dffe25c2
Grigore Stefan <g_stefan@yahoo.com>
2021-12-01 02:24:16 +0200

    Update

--- build/build.config.cmd
+++ build/build.config.cmd
@@ -4,5 +4,5 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PRODUCT_NAME=installer-hypertext-preprocessor
-set PRODUCT_VERSION=8.0.8
+set PRODUCT_VERSION=8.0.13
 set PRODUCT_BASE=hypertext-preprocessor
