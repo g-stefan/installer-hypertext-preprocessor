2c6b0b0f3d70164d54c0ae819a4208916c322e24
Grigore Stefan <g_stefan@yahoo.com>
2023-01-13 00:09:28 +0200

    Update

--- fabricare/github-release.js
+++ fabricare/github-release.js
@@ -1,6 +1,6 @@
 // Created by Grigore Stefan <g_stefan@yahoo.com>
 // Public domain (Unlicense) <http://unlicense.org>
-// SPDX-FileCopyrightText: 2021-2023 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-FileCopyrightText: 2020-2023 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
 if (Script.isNil(Solution.hasGithub)) {
--- source/composer.phar.cmd
+++ source/composer.phar.cmd
@@ -1,7 +1,7 @@
 @echo off
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 rem Public domain (Unlicense) <http://unlicense.org>
-rem SPDX-FileCopyrightText: 2021-2023 Grigore Stefan <g_stefan@yahoo.com>
+rem SPDX-FileCopyrightText: 2020-2023 Grigore Stefan <g_stefan@yahoo.com>
 rem SPDX-License-Identifier: Unlicense
 
 php "%~dp0composer.phar" %*
